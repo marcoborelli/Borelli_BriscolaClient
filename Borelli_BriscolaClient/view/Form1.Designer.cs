e562401859b3eacbc23c8d39e1e4b7505e076d17
marcoborelli <marco05.borelli@gmail.com>
2024-02-21 16:31:13 +0100

    view\Form1: implementata la form in cui si vedono le stanze esistenti e ci si può unire ad una di esse

--- Borelli_BriscolaClient/view/Form1.Designer.cs
+++ Borelli_BriscolaClient/view/Form1.Designer.cs
@@ -23,22 +23,85 @@
         /// il contenuto del metodo con l'editor di codice.
         /// </summary>
         private void InitializeComponent() {
+            this.labelName = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.lvTables = new System.Windows.Forms.ListView();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.labelDebug = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 390);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(42, 13);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "NOME:";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(181, 387);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // lvTables
+            // 
+            this.lvTables.FullRowSelect = true;
+            this.lvTables.HideSelection = false;
+            this.lvTables.Location = new System.Drawing.Point(12, 33);
+            this.lvTables.Name = "lvTables";
+            this.lvTables.Size = new System.Drawing.Size(269, 348);
+            this.lvTables.TabIndex = 0;
+            this.lvTables.UseCompatibleStateImageBehavior = false;
+            this.lvTables.View = System.Windows.Forms.View.Details;
+            this.lvTables.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvTables_MouseDoubleClick);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(12, 9);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(47, 13);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "labelInfo";
+            // 
+            // labelDebug
+            // 
+            this.labelDebug.AutoSize = true;
+            this.labelDebug.Location = new System.Drawing.Point(12, 425);
+            this.labelDebug.Name = "labelDebug";
+            this.labelDebug.Size = new System.Drawing.Size(42, 13);
+            this.labelDebug.TabIndex = 3;
+            this.labelDebug.Text = "NOME:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(300, 495);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelDebug);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.lvTables);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.ListView lvTables;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelDebug;
     }
 }
 
--- Borelli_BriscolaClient/view/Form1.cs
+++ Borelli_BriscolaClient/view/Form1.cs
@@ -1,16 +1,126 @@
 ﻿using System;
+using System.Net.Sockets;
 using System.Windows.Forms;
+using Borelli_BriscolaClient.controller;
+using System.Text.RegularExpressions;
 
 namespace Borelli_BriscolaClient.view {
     public partial class Form1 : Form {
+        private TcpClient Client { get; set; }
+
+
+        //TODO: salvare in un file di testo a parte
+        private string Ip { get; set; } = "192.168.1.114";
+        private short Port { get; set; } = 5000;
+
+
+        private bool IsUsernameOk = false;
+        private string PlayerName { get; set; }
+
         public Form1() {
             InitializeComponent();
+            labelInfo.Text = $"Connesso al socket {Ip}:{Port}";
+            Client = new TcpClient(Ip, Port);
         }
 
         private void Form1_Load(object sender, EventArgs e) {
+            Utilities.Init(GetNewCommand, Client);
+        }
+
+        private void GetNewCommand(string command) {
+            this.Invoke(new MethodInvoker(delegate {
+                labelDebug.Text = $"{DateTime.Now}: {command}\n";
+
+                if (Regex.IsMatch(command, @"^preReg:(\w+,\w+,\w+;)+$")) {
+                    labelDebug.Text=$"Entrato in preReg\n{command}";
+
+                    ShowAllTablesInListView(command);
+                }
+
+                if (command == "reg:res=ok") {
+                    IsUsernameOk = true;
+                } else if (command == "reg:res=error") {
+                    MessageBox.Show("Esiste già un utente con questo username nella stanza");
+                }
+                if (IsUsernameOk) {
+                    if (command == "reg:state=start") {
+                        //TODO
+                    } else if (Regex.IsMatch(command, @"^reg:update=(\w+;)+$")) {
+                        ShowInfoTableInListView(command);
+                    }
+                }
+            }));
+            return;
+
+
+        }
+
+
+        private void ShowAllTablesInListView(string str) {
+            ResetListView();
+            lvTables.Columns.Add("Id");
+            lvTables.Columns.Add("Partecipanti");
+
+            //preReg:<id,MaxPart,Part>;...
+            string data = str.Split(':')[1];
+
+            string[] tables = data.Split(';');
+
+            for (short i = 0; i < tables.Length; i++) {
+                string[] fields = tables[i].Split(',');
+
+                if (fields.Length < 3 || fields[1] == fields[2]) { //se una stanza e' piena non la si visualizza
+                    continue;
+                }
+
+                lvTables.Items.Add(new ListViewItem(new string[] { fields[0], $"{fields[2]}/{fields[1]}" }));
+                //id stanza; numPartecipanti/partecipantiTotali
+            }
+
+            lvTables.Refresh();
+        }
+
+        private void ShowInfoTableInListView(string str) {
+            ResetListView();
+            lvTables.Columns.Add("Username");
+
+            //reg:update=<username>;...
+            string data = str.Split('=')[1];
+
+            string[] players = data.Split(';');
+
+            for (byte i = 0; i < players.Length; i++) {
+                lvTables.Items.Add(new ListViewItem(players[i]));
+            }
+
+            lvTables.Refresh();
+        }
+
+        private void lvTables_MouseDoubleClick(object sender, MouseEventArgs e) {
+            if (lvTables.SelectedItems.Count < 1) {
+                return;
+            }
+
+            if (String.IsNullOrEmpty(tbName.Text)) {
+                MessageBox.Show("Inserire uno username");
+                return;
+            }
+
+            PlayerName = tbName.Text.Trim();
+
+            //reg:table=<id>
+            Utilities.Instance.WriteLineStream($"reg:table={lvTables.SelectedItems[0].SubItems[0].Text}");
+            Utilities.Instance.WriteLineStream($"reg:username={PlayerName}");
+        }
+
+        private void ResetListView() {
+            lvTables.Columns.Clear();
+            lvTables.Items.Clear();
+            lvTables.Refresh();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            Utilities.Instance.WriteLineStream("chiudi");
         }
     }
 }
