6ce9323239f18249839cd41b5ffb978dfec0428a
marcoborelli <marco05.borelli@gmail.com>
2024-02-25 17:51:20 +0100

    view\Game.cs: aggiunte carte per supporto a 4 giocatori   aggiunte carte che rappresentano il mazzo con sotto la briscola   aggiunta lable che mostra quante carte mancano al termine

--- Borelli_BriscolaClient/view/Game.Designer.cs
+++ Borelli_BriscolaClient/view/Game.Designer.cs
@@ -28,12 +28,22 @@
             this.bCard2 = new System.Windows.Forms.Button();
             this.lDebug = new System.Windows.Forms.Label();
             this.lBriscola = new System.Windows.Forms.Label();
+            this.bCardPlayedP3 = new System.Windows.Forms.Button();
+            this.bCardPlayedP4 = new System.Windows.Forms.Button();
+            this.bCardPlayedP1 = new System.Windows.Forms.Button();
+            this.bCardPlayedP2 = new System.Windows.Forms.Button();
+            this.bCardBriscola = new System.Windows.Forms.Button();
+            this.bCardBack = new System.Windows.Forms.Button();
+            this.lPlayer2 = new System.Windows.Forms.Label();
+            this.lPlayer3 = new System.Windows.Forms.Label();
+            this.lPlayer4 = new System.Windows.Forms.Label();
+            this.lRemCard = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // bCard3
             // 
             this.bCard3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.bCard3.Location = new System.Drawing.Point(304, 55);
+            this.bCard3.Location = new System.Drawing.Point(409, 553);
             this.bCard3.Name = "bCard3";
             this.bCard3.Size = new System.Drawing.Size(98, 154);
             this.bCard3.TabIndex = 1;
@@ -43,7 +53,7 @@
             // bCard1
             // 
             this.bCard1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.bCard1.Location = new System.Drawing.Point(200, 55);
+            this.bCard1.Location = new System.Drawing.Point(201, 553);
             this.bCard1.Name = "bCard1";
             this.bCard1.Size = new System.Drawing.Size(98, 154);
             this.bCard1.TabIndex = 2;
@@ -53,7 +63,7 @@
             // bCard2
             // 
             this.bCard2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.bCard2.Location = new System.Drawing.Point(408, 55);
+            this.bCard2.Location = new System.Drawing.Point(305, 553);
             this.bCard2.Name = "bCard2";
             this.bCard2.Size = new System.Drawing.Size(98, 154);
             this.bCard2.TabIndex = 3;
@@ -72,17 +82,129 @@
             // lBriscola
             // 
             this.lBriscola.AutoSize = true;
-            this.lBriscola.Location = new System.Drawing.Point(11, 196);
+            this.lBriscola.Location = new System.Drawing.Point(12, 694);
             this.lBriscola.Name = "lBriscola";
             this.lBriscola.Size = new System.Drawing.Size(35, 13);
             this.lBriscola.TabIndex = 5;
             this.lBriscola.Text = "label2";
             // 
+            // bCardPlayedP3
+            // 
+            this.bCardPlayedP3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bCardPlayedP3.Enabled = false;
+            this.bCardPlayedP3.Location = new System.Drawing.Point(40, 222);
+            this.bCardPlayedP3.Name = "bCardPlayedP3";
+            this.bCardPlayedP3.Size = new System.Drawing.Size(154, 98);
+            this.bCardPlayedP3.TabIndex = 6;
+            this.bCardPlayedP3.UseVisualStyleBackColor = true;
+            this.bCardPlayedP3.Visible = false;
+            // 
+            // bCardPlayedP4
+            // 
+            this.bCardPlayedP4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bCardPlayedP4.Enabled = false;
+            this.bCardPlayedP4.Location = new System.Drawing.Point(519, 222);
+            this.bCardPlayedP4.Name = "bCardPlayedP4";
+            this.bCardPlayedP4.Size = new System.Drawing.Size(154, 98);
+            this.bCardPlayedP4.TabIndex = 7;
+            this.bCardPlayedP4.UseVisualStyleBackColor = true;
+            this.bCardPlayedP4.Visible = false;
+            // 
+            // bCardPlayedP1
+            // 
+            this.bCardPlayedP1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bCardPlayedP1.Enabled = false;
+            this.bCardPlayedP1.Location = new System.Drawing.Point(305, 358);
+            this.bCardPlayedP1.Name = "bCardPlayedP1";
+            this.bCardPlayedP1.Size = new System.Drawing.Size(98, 154);
+            this.bCardPlayedP1.TabIndex = 8;
+            this.bCardPlayedP1.UseVisualStyleBackColor = true;
+            this.bCardPlayedP1.Visible = false;
+            // 
+            // bCardPlayedP2
+            // 
+            this.bCardPlayedP2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bCardPlayedP2.Enabled = false;
+            this.bCardPlayedP2.Location = new System.Drawing.Point(305, 30);
+            this.bCardPlayedP2.Name = "bCardPlayedP2";
+            this.bCardPlayedP2.Size = new System.Drawing.Size(98, 154);
+            this.bCardPlayedP2.TabIndex = 9;
+            this.bCardPlayedP2.UseVisualStyleBackColor = true;
+            this.bCardPlayedP2.Visible = false;
+            // 
+            // bCardBriscola
+            // 
+            this.bCardBriscola.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bCardBriscola.Enabled = false;
+            this.bCardBriscola.Location = new System.Drawing.Point(509, 40);
+            this.bCardBriscola.Name = "bCardBriscola";
+            this.bCardBriscola.Size = new System.Drawing.Size(154, 98);
+            this.bCardBriscola.TabIndex = 10;
+            this.bCardBriscola.UseVisualStyleBackColor = true;
+            // 
+            // bCardBack
+            // 
+            this.bCardBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bCardBack.Enabled = false;
+            this.bCardBack.Location = new System.Drawing.Point(591, 12);
+            this.bCardBack.Name = "bCardBack";
+            this.bCardBack.Size = new System.Drawing.Size(98, 154);
+            this.bCardBack.TabIndex = 11;
+            this.bCardBack.UseVisualStyleBackColor = true;
+            // 
+            // lPlayer2
+            // 
+            this.lPlayer2.AutoSize = true;
+            this.lPlayer2.Location = new System.Drawing.Point(302, 14);
+            this.lPlayer2.Name = "lPlayer2";
+            this.lPlayer2.Size = new System.Drawing.Size(42, 13);
+            this.lPlayer2.TabIndex = 12;
+            this.lPlayer2.Text = "Player2";
+            // 
+            // lPlayer3
+            // 
+            this.lPlayer3.AutoSize = true;
+            this.lPlayer3.Location = new System.Drawing.Point(12, 222);
+            this.lPlayer3.Name = "lPlayer3";
+            this.lPlayer3.Size = new System.Drawing.Size(42, 13);
+            this.lPlayer3.TabIndex = 13;
+            this.lPlayer3.Text = "Player3";
+            this.lPlayer3.Visible = false;
+            // 
+            // lPlayer4
+            // 
+            this.lPlayer4.AutoSize = true;
+            this.lPlayer4.Location = new System.Drawing.Point(675, 222);
+            this.lPlayer4.Name = "lPlayer4";
+            this.lPlayer4.Size = new System.Drawing.Size(42, 13);
+            this.lPlayer4.TabIndex = 14;
+            this.lPlayer4.Text = "Player4";
+            this.lPlayer4.Visible = false;
+            // 
+            // lRemCard
+            // 
+            this.lRemCard.AutoSize = true;
+            this.lRemCard.Location = new System.Drawing.Point(596, 169);
+            this.lRemCard.Name = "lRemCard";
+            this.lRemCard.Size = new System.Drawing.Size(35, 13);
+            this.lRemCard.TabIndex = 15;
+            this.lRemCard.Text = "label1";
+            // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(722, 238);
+            this.ClientSize = new System.Drawing.Size(722, 716);
+            this.Controls.Add(this.lRemCard);
+            this.Controls.Add(this.lPlayer4);
+            this.Controls.Add(this.lPlayer3);
+            this.Controls.Add(this.lPlayer2);
+            this.Controls.Add(this.bCardBack);
+            this.Controls.Add(this.bCardBriscola);
+            this.Controls.Add(this.bCardPlayedP2);
+            this.Controls.Add(this.bCardPlayedP1);
+            this.Controls.Add(this.bCardPlayedP4);
+            this.Controls.Add(this.bCardPlayedP3);
             this.Controls.Add(this.lBriscola);
             this.Controls.Add(this.lDebug);
             this.Controls.Add(this.bCard2);
@@ -102,5 +224,15 @@
         private System.Windows.Forms.Button bCard2;
         private System.Windows.Forms.Label lDebug;
         private System.Windows.Forms.Label lBriscola;
+        private System.Windows.Forms.Button bCardPlayedP3;
+        private System.Windows.Forms.Button bCardPlayedP4;
+        private System.Windows.Forms.Button bCardPlayedP1;
+        private System.Windows.Forms.Button bCardPlayedP2;
+        private System.Windows.Forms.Button bCardBriscola;
+        private System.Windows.Forms.Button bCardBack;
+        private System.Windows.Forms.Label lPlayer2;
+        private System.Windows.Forms.Label lPlayer3;
+        private System.Windows.Forms.Label lPlayer4;
+        private System.Windows.Forms.Label lRemCard;
     }
 }
--- Borelli_BriscolaClient/view/Game.cs
+++ Borelli_BriscolaClient/view/Game.cs
@@ -9,6 +9,8 @@ using Borelli_BriscolaClient.controller;
 namespace Borelli_BriscolaClient.view {
     public partial class Game : Form {
         List<Button> Hand = new List<Button>();
+        List<Button> OtherPlayers = new List<Button>();
+
         private string PlayerName { get; set; }
         public Game(string playerName) {
             InitializeComponent();
@@ -16,9 +18,16 @@ namespace Borelli_BriscolaClient.view {
 
             Hand.ForEach(x => x.MouseClick += BCard_MouseClick);
 
+            //rivedere
+            OtherPlayers.AddRange(new Button[] { bCardPlayedP2, bCardPlayedP1, bCardPlayedP3, bCardPlayedP4 });
+            bCardPlayedP1.Name = PlayerName;
+
             PlayerName = playerName;
+            Text = playerName;
 
             Utilities.ChangeDelegatedFunction(GetNewCommand);
+
+            bCardBack.BackgroundImage = Image.FromFile($"../../../images/back.jpg");
         }
 
         private void BCard_MouseClick(object sender, MouseEventArgs e) {
@@ -26,20 +35,42 @@ namespace Borelli_BriscolaClient.view {
             Utilities.Instance.WriteLineStream($"play:cardToPlay={btn.Name}");
 
             btn.Visible = false;
+
+            bCardPlayedP1.BackgroundImage = btn.BackgroundImage;
+            bCardPlayedP1.Visible = true;
         }
 
         private void GetNewCommand(string command) {
             this.Invoke(new MethodInvoker(delegate {
-                lDebug.Text = $"{DateTime.Now}: '{command}'";
-                if (Regex.IsMatch(command, @"^play:briscola=\w+_\w+$")) {
-                    lBriscola.Text = command.Split('=')[1];
+                lDebug.Text = $"'{command}'";
+                if (Regex.IsMatch(command, @"^play:players=(\w+;)+$")) {
+                    List<string> players = command.Remove(command.Length - 1, 1).Split('=')[1].Split(';').ToList(); //rimuovo il ';' in fondo
+                    InitOtherPlayers(players);
+                } else if (Regex.IsMatch(command, @"^play:briscola=\w+_\w+$")) {
+                    string briscola = command.Split('=')[1];
+
+                    lBriscola.Text = briscola;
+                    bCardBriscola.BackgroundImage = GetRotatedImage($"../../../images/{briscola}.jpg");
                 } else if (Regex.IsMatch(command, @"^play:cardDrawed=\w+_\w+$")) {
                     GiveToFreeHandButtonCard(command.Split('=')[1]);
                 } else if (Regex.IsMatch(command, @"^play:turn=\w+$")) {
                     Hand.ForEach(x => x.Enabled = (command.Split('=')[1] == PlayerName));
-                } else if (Regex.IsMatch(command, @"^play:cardPlayed=(\w+_\w+);player=\w+$")) {
-                    //other things
-                    //Hand.ForEach(x => x.Enabled = true);
+                } else if (Regex.IsMatch(command, @"^play:cardPlayed=(\w+_\w+);player=(\w+)$")) {
+                    string playerName = command.Split(';')[1].Split('=')[1];
+                    string nameCard = command.Split('=')[1].Split(';')[0];
+
+                    GiveToPlayerButtonCard(playerName, nameCard);
+                } else if (Regex.IsMatch(command, @"^play:handWinner=\w+$")) {
+                    bCardPlayedP1.Visible = false; //resetto il tavolo con le carte giocate
+                    OtherPlayers.ForEach(x => x.Visible = false);
+                } else if (Regex.IsMatch(command, @"^play:cardRemaingNum=([0-9]+)$")) {
+                    int remCard = int.Parse(command.Split('=')[1]);
+
+                    if (remCard > 0) {
+                        lRemCard.Text = $"{remCard}";
+                    } else {
+                        lRemCard.Visible = bCardBack.Visible = bCardBriscola.Visible = false;
+                    }
                 }
 
                 if (Regex.IsMatch(command, @"^end:winner=(\w+;)+$")) {
@@ -51,6 +82,50 @@ namespace Borelli_BriscolaClient.view {
             return;
         }
 
+        public void WriteVerticalLable(Label l, string text) {
+            l.Text = "";
+            for (short i = 0; i < text.Length; i++) {
+                l.Text += $"{text[i]}\n";
+            }
+        }
+
+        private void InitOtherPlayers(List<string> players) {
+            /*
+                  Player2
+             Player3    Player4
+                  Player1
+            */
+            byte ownPosition = (byte)players.IndexOf(PlayerName);
+
+            if (players.Count == 4) {
+                lPlayer3.Visible = lPlayer4.Visible = lPlayer2.Visible = true;
+
+                WriteVerticalLable(lPlayer3, players[GetIndexIntoRange((ownPosition - 1), players.Count)]);
+                bCardPlayedP3.Name = players[GetIndexIntoRange((ownPosition - 1), players.Count)];
+
+                WriteVerticalLable(lPlayer4, players[GetIndexIntoRange((ownPosition + 1), players.Count)]);
+                bCardPlayedP4.Name = players[GetIndexIntoRange((ownPosition + 1), players.Count)];
+
+                lPlayer2.Text = players[GetIndexIntoRange((ownPosition + 2), players.Count)];
+                bCardPlayedP2.Name = players[GetIndexIntoRange((ownPosition + 2), players.Count)];
+            } else if (players.Count == 2) {
+                OtherPlayers.Remove(bCardPlayedP3);
+                OtherPlayers.Remove(bCardPlayedP4);
+
+                lPlayer2.Visible = true;
+                lPlayer2.Text = players[GetIndexIntoRange((ownPosition + 1), players.Count)];
+                bCardPlayedP2.Name = players[GetIndexIntoRange((ownPosition + 1), players.Count)];
+            }
+        }
+
+        private int GetIndexIntoRange(int ownIndex, int membersCount) { //ottiene la posizione degli altri giocatori rispetto al giocatore che sta giocando su quel client
+            if (ownIndex < 0) { //se e' negativo riparto dal massimo e sottraggo quelli di cui andavo sotto
+                ownIndex = membersCount + ownIndex;
+            }
+
+            return ownIndex < membersCount ? ownIndex : (ownIndex - membersCount);
+        }
+
         private void GiveToFreeHandButtonCard(string nameCard) {
             Button free = Hand.First(x => !x.Visible);
 
@@ -59,8 +134,22 @@ namespace Borelli_BriscolaClient.view {
             }
 
             free.BackgroundImage = Image.FromFile($"../../../images/{nameCard}.jpg");
+
             free.Name = nameCard;
             free.Visible = true;
+        }
+
+        private void GiveToPlayerButtonCard(string playerName, string card) {
+            Button tmp = OtherPlayers.First(x => x.Name == playerName);
+            tmp.BackgroundImage = tmp.Size.Height > tmp.Size.Width ? Image.FromFile($"../../../images/{card}.jpg") : GetRotatedImage($"../../../images/{card}.jpg"); //se e' verticale non la ruoto
+            tmp.Visible = true;
+        }
+
+        private Image GetRotatedImage(string path) {
+            Image imm = Image.FromFile(path);
+            imm.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            return imm;
         }
     }
 }
