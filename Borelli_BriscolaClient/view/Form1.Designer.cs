a56433d56099fcf1cb0499e899f7690da11671e3
marcoborelli <marco05.borelli@gmail.com>
2024-02-25 21:18:15 +0100

    view\Form1: rifatta grafica

--- Borelli_BriscolaClient/view/Form1.Designer.cs
+++ Borelli_BriscolaClient/view/Form1.Designer.cs
@@ -33,12 +33,16 @@
             this.cbRoomNum = new System.Windows.Forms.ComboBox();
             this.bCreateRoom = new System.Windows.Forms.Button();
             this.bUpdateList = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(12, 390);
+            this.labelName.Location = new System.Drawing.Point(9, 393);
             this.labelName.Name = "labelName";
             this.labelName.Size = new System.Drawing.Size(42, 13);
             this.labelName.TabIndex = 4;
@@ -46,7 +50,7 @@
             // 
             // tbName
             // 
-            this.tbName.Location = new System.Drawing.Point(181, 387);
+            this.tbName.Location = new System.Drawing.Point(178, 390);
             this.tbName.Name = "tbName";
             this.tbName.Size = new System.Drawing.Size(100, 20);
             this.tbName.TabIndex = 2;
@@ -55,7 +59,7 @@
             // 
             this.lvTables.FullRowSelect = true;
             this.lvTables.HideSelection = false;
-            this.lvTables.Location = new System.Drawing.Point(12, 33);
+            this.lvTables.Location = new System.Drawing.Point(9, 36);
             this.lvTables.Name = "lvTables";
             this.lvTables.Size = new System.Drawing.Size(269, 348);
             this.lvTables.TabIndex = 0;
@@ -66,7 +70,7 @@
             // labelInfo
             // 
             this.labelInfo.AutoSize = true;
-            this.labelInfo.Location = new System.Drawing.Point(12, 9);
+            this.labelInfo.Location = new System.Drawing.Point(9, 12);
             this.labelInfo.Name = "labelInfo";
             this.labelInfo.Size = new System.Drawing.Size(47, 13);
             this.labelInfo.TabIndex = 1;
@@ -75,7 +79,7 @@
             // labelDebug
             // 
             this.labelDebug.AutoSize = true;
-            this.labelDebug.Location = new System.Drawing.Point(9, 473);
+            this.labelDebug.Location = new System.Drawing.Point(12, 503);
             this.labelDebug.Name = "labelDebug";
             this.labelDebug.Size = new System.Drawing.Size(37, 13);
             this.labelDebug.TabIndex = 3;
@@ -84,7 +88,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 421);
+            this.label1.Location = new System.Drawing.Point(9, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 13);
             this.label1.TabIndex = 5;
@@ -92,7 +96,7 @@
             // 
             // tbRoomName
             // 
-            this.tbRoomName.Location = new System.Drawing.Point(15, 437);
+            this.tbRoomName.Location = new System.Drawing.Point(12, 23);
             this.tbRoomName.Name = "tbRoomName";
             this.tbRoomName.Size = new System.Drawing.Size(170, 20);
             this.tbRoomName.TabIndex = 6;
@@ -104,14 +108,14 @@
             this.cbRoomNum.Items.AddRange(new object[] {
             "2",
             "4"});
-            this.cbRoomNum.Location = new System.Drawing.Point(191, 437);
+            this.cbRoomNum.Location = new System.Drawing.Point(188, 23);
             this.cbRoomNum.Name = "cbRoomNum";
             this.cbRoomNum.Size = new System.Drawing.Size(59, 21);
             this.cbRoomNum.TabIndex = 7;
             // 
             // bCreateRoom
             // 
-            this.bCreateRoom.Location = new System.Drawing.Point(256, 435);
+            this.bCreateRoom.Location = new System.Drawing.Point(253, 21);
             this.bCreateRoom.Name = "bCreateRoom";
             this.bCreateRoom.Size = new System.Drawing.Size(25, 22);
             this.bCreateRoom.TabIndex = 8;
@@ -121,7 +125,7 @@
             // 
             // bUpdateList
             // 
-            this.bUpdateList.Location = new System.Drawing.Point(235, 4);
+            this.bUpdateList.Location = new System.Drawing.Point(232, 7);
             this.bUpdateList.Name = "bUpdateList";
             this.bUpdateList.Size = new System.Drawing.Size(46, 23);
             this.bUpdateList.TabIndex = 9;
@@ -129,25 +133,47 @@
             this.bUpdateList.UseVisualStyleBackColor = true;
             this.bUpdateList.Click += new System.EventHandler(this.bUpdateList_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.lvTables);
+            this.panel1.Controls.Add(this.bUpdateList);
+            this.panel1.Controls.Add(this.labelInfo);
+            this.panel1.Controls.Add(this.tbName);
+            this.panel1.Controls.Add(this.labelName);
+            this.panel1.Location = new System.Drawing.Point(15, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(290, 430);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.tbRoomName);
+            this.panel2.Controls.Add(this.bCreateRoom);
+            this.panel2.Controls.Add(this.cbRoomNum);
+            this.panel2.Location = new System.Drawing.Point(15, 446);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(290, 50);
+            this.panel2.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 495);
-            this.Controls.Add(this.bUpdateList);
-            this.Controls.Add(this.bCreateRoom);
-            this.Controls.Add(this.cbRoomNum);
-            this.Controls.Add(this.tbRoomName);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.labelName);
+            this.ClientSize = new System.Drawing.Size(324, 539);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.labelDebug);
-            this.Controls.Add(this.tbName);
-            this.Controls.Add(this.labelInfo);
-            this.Controls.Add(this.lvTables);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,6 +191,8 @@
         private System.Windows.Forms.ComboBox cbRoomNum;
         private System.Windows.Forms.Button bCreateRoom;
         private System.Windows.Forms.Button bUpdateList;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
